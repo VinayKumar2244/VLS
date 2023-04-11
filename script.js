de52285da36d2315fb68fb16ea438840026869ee
VinayKumar2244 <67969596+VinayKumar2244@users.noreply.github.com>
2023-04-11 15:11:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+    $('.navbar-toggler').click(function(){
+        $('.navbar-collapse').slideToggle(300);
+    });
+    
+    smallScreenMenu();
+    let temp;
+    function resizeEnd(){
+        smallScreenMenu();
+    }
+
+    $(window).resize(function(){
+        clearTimeout(temp);
+        temp = setTimeout(resizeEnd, 100);
+        resetMenu();
+    });
+});
+
+
+const subMenus = $('.sub-menu');
+const menuLinks = $('.menu-link');
+
+function smallScreenMenu(){
+    if($(window).innerWidth() <= 992){
+        menuLinks.each(function(item){
+            $(this).click(function(){
+                $(this).next().slideToggle();
+            });
+        });
+    } else {
+        menuLinks.each(function(item){
+            $(this).off('click');
+        });
+    }
+}
+
+function resetMenu(){
+    if($(window).innerWidth() > 992){
+        subMenus.each(function(item){
+            $(this).css('display', 'none');
+        });
+    }
+}
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+    showDivs(slideIndex += n);
+
+}
+
+function showDivs(n) {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    if (n > x.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = x.length }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[slideIndex - 1].style.display = "block";
+}
